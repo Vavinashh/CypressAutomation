a9fcde71179c61777b668b2566bab26d30d6afe8
vavinashh <vavinashh@gmail.com>
2023-05-19 11:44:30 -0500

    Added Test

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,1 +1,11 @@
-Avinash1
+describe('My First Test Suite', function ()
+{
+
+    it('My First Test Case',function(){
+
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        
+    })
+
+
+})
