fc65024f885c935df5bd48d430317733001c87fc
vavinashh <vavinashh@gmail.com>
2023-10-26 10:31:02 -0500

    added new Test

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,11 +1,39 @@
-describe('My First Test Suite', function ()
-{
+/// <reference types="Cypress" />
+describe('My First Test Suite', function () {
 
-    it('My First Test Case',function(){
+    it('My First Test Case', function () {
 
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-        
-    })
+        cy.get(".search-keyword").type('Ca');
+        cy.wait(2000);
+        cy.get('.product').should('have.length', 5);
+        //for visible products only
+        cy.wait(2000);
+        cy.get('.product:visible').should('have.length', 4);
+        //parent and child chaining 
+        cy.wait(2000);
+        cy.get('.products').find('.product').should('have.length', 4);
 
+        // select or click on element
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
 
-})
+        //Select a element in a loop
+
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+ 
+            const textVeg=$el.find('h4.product-name').text()
+            if(textVeg.includes('Cashews'))
+            {
+            cy.wrap($el).find('button').click()
+            }
+            })
+
+        // Select and display text
+
+        cy.get('.brand').then(function(logoelement)
+        {
+            cy.log(logoelement.text())
+        })
+
+    });
+});
