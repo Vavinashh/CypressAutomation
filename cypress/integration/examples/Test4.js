7b8b32c36891d12eaa7de5b22f140b780cd74c91
vavinashh <vavinashh@gmail.com>
2023-11-02 11:09:08 -0500

    update

---
+++ cypress/integration/examples/Test4.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+describe('My Fourth Test Suite', function() 
+{
+ 
+it('My Fourth Test case',function() {
+ 
+//Check boxes
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+
+})
+
+})
